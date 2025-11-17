ed702bb0f6cb70e8fdd5069939e9dfebd6b3bf55
Mahiryo <ma.hiro.yogi.carvalho@gmail.com>
2025-11-17 16:42:43 -0300

    ADD API VIZ

---
+++ Pastas pessoais/MarcHiro/API_VIZ/web-data-viz/src/controllers/usuarioController.js
@@ -0,0 +1,86 @@
+var usuarioModel = require("../models/usuarioModel");
+// var aquarioModel = require("../models/aquarioModel");
+
+function autenticar(req, res) {
+  var email = req.body.emailServer;
+  var senha = req.body.senhaServer;
+
+  if (email == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (senha == undefined) {
+    res.status(400).send("Sua senha está indefinida!");
+  } else {
+    usuarioModel
+      .autenticar(email, senha)
+      .then(function (resultadoAutenticar) {
+        console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+        console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+        if (resultadoAutenticar.length == 1) {
+          console.log(resultadoAutenticar);
+            res.json({
+              idEmpresa: resultadoAutenticar[0].idEmpresa,
+              email: resultadoAutenticar[0].email,
+              razaoSocial: resultadoAutenticar[0].razaoSocial,
+              senha: resultadoAutenticar[0].senha,
+            });
+        } else if (resultadoAutenticar.length == 0) {
+          res.status(403).send("Email e/ou senha inválido(s)");
+        } else {
+          res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+        }
+      })
+      .catch(function (erro) {
+        console.log(erro);
+        console.log(
+          "\nHouve um erro ao realizar o login! Erro: ",
+          erro.sqlMessage
+        );
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
+}
+
+function cadastrar(req, res) {
+  // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+
+  // empresaServer: empresaVar,
+  // cnpjServer: cnpjVar,
+  // emailServer: emailVar,
+  // senhaServer: senhaVar
+  var empresa = req.body.empresaServer;
+  var cnpj = req.body.cnpjServer;
+  var email = req.body.emailServer;
+  var senha = req.body.senhaServer;
+
+  // Faça as validações dos valores
+  if (empresa == undefined) {
+    res.status(400).send("Seu empresa está undefined!");
+  } else if (email == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (senha == undefined) {
+    res.status(400).send("Sua senha está undefined!");
+  } else if (cnpj == undefined) {
+    res.status(400).send("Sua empresa a vincular está undefined!");
+  } else {
+    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+    usuarioModel
+      .cadastrar(empresa, email, senha, cnpj)
+      .then(function (resultado) {
+        res.json(resultado);
+      })
+      .catch(function (erro) {
+        console.log(erro);
+        console.log(
+          "\nHouve um erro ao realizar o cadastro! Erro: ",
+          erro.sqlMessage
+        );
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
+}
+
+module.exports = {
+  autenticar,
+  cadastrar,
+};
